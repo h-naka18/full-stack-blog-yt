eaec5be746a52dd69387ff3e0f7df987b1519d06
Hitoshi <h.naka18@gmail.com>
2024-01-12 20:09:40 +0900

    🐛 お試し

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        ROOT: process.env.NODE_ENV === 'production' ? `https://full-stack-blog-rdb7hlwqt-hitoshis-projects.vercel.app` : `http://localhost:3000`
+        ROOT: process.env.NODE_ENV === 'production' ? `https://full-stack-blog-kui24po75-hitoshis-projects.vercel.app` : `http://localhost:3000`
     }
 }
 
