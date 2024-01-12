d14ddd3f5f2833e5ef9ca37bfd4873031fc05872
Hitoshi <h.naka18@gmail.com>
2024-01-13 00:53:46 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,16 @@ import Image from 'next/image'
 import Link from 'next/link';
 import { PostType } from './types';
 
-async function fetchAllBlogs() {
-  const res = await fetch(`${process.env.ROOT}/api/blog`, {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  return data.posts;
-}
+// async function fetchAllBlogs() {
+//   const res = await fetch(`${process.env.ROOT}/api/blog`, {
+//     cache: "no-store",
+//   });
+//   const data = await res.json();
+//   return data.posts;
+// }
 
 export default async function Home() {
-  const posts = await fetchAllBlogs();
+  // const posts = await fetchAllBlogs();
 
   return (
     <main className="w-full h-full">
@@ -31,35 +31,6 @@ export default async function Home() {
       </div>
 
       <div className="w-full flex flex-col justify-center items-center">
-        {posts.map((post: PostType) => (
-          <div key={post.id} className="w-3/4 p-4 rounded-md mx-3 my-2 bg-slate-500 flex flex-col justify-center">
-          <div className="flex items-center my-3">
-            <div className="mr-auto">
-              <h2 className="mr-auto font-semibold text-slate-50">
-                {post.title}
-              </h2>
-            </div>
-            <Link
-              href={`/blog/edit/${post.id}`}
-              className="px-4 py-1 text-center text-xl bg-slate-900 rounded-md font-semibold text-slate-200"
-            >
-              編集
-            </Link>
-          </div>
-
-          <div className="mr-auto my-1">
-            <blockquote className="font-bold text-slate-200">
-              {new Date(post.date).toDateString()}
-            </blockquote>
-          </div>
-
-          <div className="mr-auto my-1">
-            <h2 className="text-slate-50">
-              {post.description}
-            </h2>
-          </div>
-        </div>
-      ))}
       </div>
     </main>
   )
