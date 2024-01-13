12a44471d2f3e2568bac1461181f15b78a5111fc
Hitoshi <h.naka18@gmail.com>
2024-01-13 10:40:11 +0900

    apiのurl変更

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        ROOT: process.env.NODE_ENV === 'production' ? `https://full-stack-blog-yt.vercel.app` : `http://localhost:3000`
+        // ROOT: process.env.NODE_ENV === 'production' ? `https://full-stack-blog-yt.vercel.app` : `http://localhost:3000`
+        ROOT: process.env.NODE_ENV === 'production' ? `full-stack-blog-1czljgjop-hitoshis-projects.vercel.app` : `http://localhost:3000`
     }
 }
 
